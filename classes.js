41a4636e0409514122502278424310dd0cf22546
Btaft6278 <btaft6278@gmail.com>
2022-05-23 19:28:56 -0500

    eod

--- classes.js
+++ classes.js
@@ -25,3 +25,12 @@ class Person {
   o1.greet();
   o2.greet();
   o3.greet();
+
+  const o1 = new Person("Max");
+  const o2 = new Student("Tina", "1st Grade");
+  const o3 = new Student("Mary", "2nd Grade");
+  o3.greet = () => console.log('I am special!');
+  
+  o1.greet();
+  o2.greet();
+  o3.greet();
